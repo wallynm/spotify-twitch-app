8cac8a0a02637b9f6800d54d24b63945c225d5e3
Wallysson Nunes <wally.nm@gmail.com>
2021-03-05 22:35:43 -0300

    Added new styles

---
+++ components/Layout/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Layout = (props) => {
+  const control = false;
+
+  return (
+    <div>
+      {props.children}
+    </div>
+  )
+}
+
+export default Layout;
---
+++ pages/home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Layout from '../components/Layout/index'
+
+
+const Home = () => {
+
+  return (
+    <div>
+      <Layout>
+        <div style={{background: 'red'}}>
+          Spotify App Home
+        </div>
+        <div style={{background: 'green'}}>
+          Spotify App Home
+        </div>
+      </Layout>
+    </div>
+  )
+}
+
+export default Home;
